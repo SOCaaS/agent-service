51a2af76d49a92eddf251ab814496351de379e04
ezeutno <ivan.suratno@gmail.com>
2021-03-28 08:50:15 +0000

    Fix Typo

--- start.sh
+++ start.sh
@@ -57,7 +57,8 @@ apt update
 apt install suricata suricata-dbg -y
 
 sed -i "s/{{ HOST_IP }}/$HOST_IP/g" suricata.yaml
-sed -i "s/{{ INTERFACE }}/$INTERFACE/g" suricata.yml
+
+sed -i "s/{{ INTERFACE }}/$INTERFACE/g" suricata.yaml
 
 cp detect-dos.rules /etc/suricata/rules
 
