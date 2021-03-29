c7affca163024bc86edb9b055368523a0ae920ad
ezeutno <ivan.suratno@gmail.com>
2021-03-29 08:46:50 +0000

    Turn off agent service

--- deployment/main.tf
+++ deployment/main.tf
@@ -7,10 +7,10 @@ terraform {
 module "digitalocean" {
     source              = "./droplet"
     servers             = [
-        {
-            name = "agent-deployment",
-            type = "s-2vcpu-2gb"
-        }
+        # {
+        #     name = "agent-deployment",
+        #     type = "s-2vcpu-2gb"
+        # }
     ]
     public_key_name     = var.public_key_name
     digital_ocean_key   = var.digital_ocean_key
